d19d7a13bc35c8d4014a69b594d40d33dd476b44
Yangirov Denis <yangirovdf@gmail.com>
2022-11-25 15:25:02 +0300

    Рабочая версия для Arctic For

--- app/src/main/java/com/example/testroom2/data/Word.kt
+++ app/src/main/java/com/example/testroom2/data/Word.kt
@@ -4,8 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * @ Entity - You must annotate the class as an entity and supply a table name if not class name.
+ * @ PrimaryKey - You must identify the primary key.
+ * @ ColumnInfo - You must supply the column name if it is different from the variable name.
+ * */
+
 @Entity(tableName = "word_table")
-class Word(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "word") val word: String
-)
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
