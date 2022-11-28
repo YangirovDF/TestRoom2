28e624a2c062a76e0d91dfb090f38c34120df2ac
Yangirov Denis <yangirovdf@gmail.com>
2022-11-28 14:11:58 +0300

    Добавил маску для приложения

--- app/src/main/java/com/example/testroom2/presentation/NewWordActivity.kt
+++ app/src/main/java/com/example/testroom2/presentation/NewWordActivity.kt
@@ -8,6 +8,12 @@ import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import com.example.testroom2.R
+import ru.tinkoff.decoro.MaskImpl
+import ru.tinkoff.decoro.parser.UnderscoreDigitSlotsParser
+import ru.tinkoff.decoro.slots.PredefinedSlots
+import ru.tinkoff.decoro.slots.Slot
+import ru.tinkoff.decoro.watchers.FormatWatcher
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher
 
 class NewWordActivity : AppCompatActivity() {
 
@@ -15,6 +21,16 @@ class NewWordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_word)
         val editWordView = findViewById<EditText>(R.id.edit_word)
+
+        val myMask: Array<out Slot> = UnderscoreDigitSlotsParser().parseSlots("8 800 555 35 35")
+        val formatWatcher: FormatWatcher = MaskFormatWatcher(
+            MaskImpl.createNonTerminated(PredefinedSlots.RUS_PHONE_NUMBER)
+        )
+        val formatWatcher2: FormatWatcher = MaskFormatWatcher(
+            MaskImpl.createNonTerminated(myMask)
+        )
+
+        formatWatcher.installOn(editWordView)
 
         val button = findViewById<Button>(R.id.button_save)
         button.setOnClickListener {
